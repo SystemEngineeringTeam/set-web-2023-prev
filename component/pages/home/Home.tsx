a679b6271e85f27e794239f1aac908f2689012ad
SatooRu65536 <ryuto4619@gmail.com>
2023-10-19 15:49:00 +0900

    add: about 追加

--- component/pages/home/Home.tsx
+++ component/pages/home/Home.tsx
@@ -1,9 +1,11 @@
 import TopSection from "@/component/pages/home/sections/top/Top";
+import About from "./sections/about/About";
 
 export default function Home() {
   return (
     <main>
       <TopSection />
+      <About />
     </main>
   );
 }
---
+++ component/pages/home/sections/about/About.tsx
@@ -0,0 +1,33 @@
+import { DescriptionContents, DescriptionTitle } from "@/const/md/home";
+import Color from "@/const/style/Color";
+import styled from "styled-components";
+
+const AboutSection = styled.section`
+  height: auto;
+  padding: 40px 0;
+  background-color: ${Color.WHITE};
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  padding: 10px 0;
+  margin-inline: auto;
+  max-width: 850px;
+`;
+
+const Description = styled.p`
+  margin-inline: auto;
+  max-width: 800px;
+  font-size: 1rem;
+`;
+
+export default function About() {
+  return (
+    <AboutSection>
+      <Title>{DescriptionTitle}</Title>
+      {DescriptionContents.map((desc, i) => (
+        <Description key={i}>{desc}</Description>
+      ))}
+    </AboutSection>
+  );
+}
